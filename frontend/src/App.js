d9e2a67bcd5d8e300ed46f6dbcb8c7f117311e4e
Jami Karvanen <jami.karvanen@yle.fi>
2021-03-20 22:38:56 +0200

    Usercount widget & style fixes (#2)
    * Implement live user count in top-left corner
    
    * Fix message container height and add padding to usercount
    
    * Fix messages container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,25 @@ import React from 'react'
 import './App.css'
 import Chat from './Chat'
 import Video from './Video'
+import UserCount from './UserCount'
 
-import { fetchMessages, sendMessage, receiveMessage } from './network'
+import { fetchMessages, sendMessage, registerMessageHandlers } from './network'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { messages: [] };
+    this.state = { messages: [], userCount: 0 };
   }
   componentDidMount() {
     fetchMessages().then(messages=> {
       this.setState({
         messages
       });
-      receiveMessage(this.onReceiveMessage.bind(this))
+      registerMessageHandlers(this.onReceiveMessage.bind(this), this.onUserCountChange.bind(this))
     });
+  }
+  onUserCountChange(count) {
+    this.setState({ userCount: count})
   }
   onReceiveMessage(message) {
     this.setState({ messages: [...this.state.messages, message] })
@@ -27,6 +31,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <UserCount count={this.state.userCount} />
         <Video/>
         <Chat onSendMessage={this.onSendMessage.bind(this)} messages={this.state.messages} />
       </div>
